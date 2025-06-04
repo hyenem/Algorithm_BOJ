c178e14d0417c6515c94bc410cee19fafc74b405
hyenem <gpals04291@gmail.com>
2025-06-05 08:18:12 +0900

    [Gold III] Title: 금민수의 합, Time: 888 ms, Memory: 159312 KB -BaekjoonHub

---
+++ 백준/Gold/1528. 금민수의 합/금민수의 합.py
@@ -0,0 +1,44 @@
+from collections import deque
+
+def find_goldminsu(num):
+    if num > N:
+        return
+    if num != 0:
+        goldminsu.append(num)
+    for digit in (4, 7):
+        find_goldminsu(num * 10 + digit)
+
+N = int(input())
+
+goldminsu = []
+find_goldminsu(0)
+goldminsu.sort()
+
+dp = [-1] * (N + 1)
+prev = [-1] * (N + 1)
+used = [-1] * (N + 1)
+
+dp[0] = 0
+q = deque([0])
+
+while q:
+    cur = q.popleft()
+    for gold in goldminsu:
+        nxt = cur + gold
+        if nxt > N:
+            continue
+        if dp[nxt] == -1 or dp[nxt] > dp[cur] + 1:
+            dp[nxt] = dp[cur] + 1
+            prev[nxt] = cur
+            used[nxt] = gold
+            q.append(nxt)
+
+if dp[N] == -1:
+    print(-1)
+else:
+    path = []
+    cur = N
+    while cur != 0:
+        path.append(used[cur])
+        cur = prev[cur]
+    print(*reversed(path))
