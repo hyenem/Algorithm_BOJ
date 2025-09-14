35bfd7a339f8fce813afcbfed42200b3d2ec56cf
hyenem <gpals04291@gmail.com>
2025-09-14 17:22:32 +0900

    [Silver II] Title: 최대 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.cc
@@ -0,0 +1,29 @@
+#include <algorithm>
+#include <queue>
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    priority_queue<int> heap;
+    int N, num;
+    cin >> N;
+
+    for(int tc=0; tc<N; tc++){
+        cin >> num;
+        if(num){
+            heap.push(num);
+        } else {
+            if(heap.empty()){
+                cout << 0 << '\n';
+            } else {
+                cout << heap.top() << '\n';
+                heap.pop();
+            }
+        }
+    }
+    return 0;
+}
